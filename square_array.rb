0a64e47ddf73ee5aec5e5d3ab4f0fe6159af4ff8
Bethany Jenkins <bethanykjenkins@rocketmail.com>
2019-09-21 21:47:16 +0000

    Automatically backed up by Learn

--- square_array.rb
+++ square_array.rb
@@ -3,3 +3,13 @@ def square_array(array)
   numbers.each do |number|
     
 end
+
+def square_array(array)
+   squared_numbers=[]
+   array.each do|numbers|
+   squared_numbers << numbers ** 2
+   end
+   
+   return squared_numbers
+   
+end
